1e3915f03974c4c1d090902745c88343e709e7c0
Brandonian <offSpring4088>
2021-07-30 19:34:08 -0700

    Handle complete and cancel events with service

--- src/app/training/training.service.ts
+++ src/app/training/training.service.ts
@@ -11,11 +11,24 @@ export class TrainingService{
   ];
 
   private runningExercise: Exercise;
+  private exercises: Exercise[] = [];
 
 
   startExercise(selectedId: string) {
     this.runningExercise = this.availableExercises.find(ex => ex.id === selectedId);
     this.exerciseChanged.next({...this.runningExercise});
+  }
+
+  completeExercise() {
+    this.exercises.push({ ...this.runningExercise, date: new Date(), state: 'completed' });
+    this.runningExercise = null;
+    this.exerciseChanged.next(null);
+  }
+
+  cancelExercise(progress: number) {
+    this.exercises.push({ ...this.runningExercise, duration: this.runningExercise.duration * (progress / 100), calories: this.runningExercise.duration * (progress / 100), date: new Date(), state: 'cancelled' });
+    this.runningExercise = null;
+    this.exerciseChanged.next(null);
   }
 
   getAvailableExercises() {
